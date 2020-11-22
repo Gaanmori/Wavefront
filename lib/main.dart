b90a35623d60d414049bb3e857cdb2551d24d5b1
Gaanmori <49103581+Gaanmori@users.noreply.github.com>
2020-11-22 15:10:28 -0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto_final/pages/wavefront.dart';
+
+
+void main() {
+  runApp( MaterialApp(
+    home: FinalProyec(),
+  ));
+}
+
+class FinalProyec extends StatefulWidget {
+  @override
+  _UTBShortPhats createState() => _UTBShortPhats();
+}
+
+class _UTBShortPhats extends State<FinalProyec> with TickerProviderStateMixin {
+  TabController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller =  TabController(length: 1,vsync: this);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+        appBar:  AppBar(
+          title:  Text("WaveFront"),
+          backgroundColor: Colors.black,
+          bottom:  TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon:  Icon(Icons.router),
+              ),
+            ],
+            controller: controller,
+          ),
+        ),
+        body:  TabBarView(
+            children: <Widget>[
+              Wavefront(),
+            ],
+            controller: controller
+        )
+    );
+  }
+}
