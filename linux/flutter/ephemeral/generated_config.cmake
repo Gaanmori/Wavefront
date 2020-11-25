06f5efbd5897abe755b66ae1b5afc14b44ee4b32
Gaanmori <49103581+Gaanmori@users.noreply.github.com>
2020-11-24 20:49:30 -0500

    Add files via upload

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/gaanmori/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/gaanmori/AndroidStudioProjects/flutter_app" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=\"/home/gaanmori/snap/flutter/common/flutter\""
+  "PROJECT_DIR=\"/home/gaanmori/AndroidStudioProjects/flutter_app\""
+  "DART_DEFINES=\"flutter.inspector.structuredErrors%3Dtrue\""
+  "DART_OBFUSCATION=\"false\""
+  "TRACK_WIDGET_CREATION=\"true\""
+  "TREE_SHAKE_ICONS=\"false\""
+  "PACKAGE_CONFIG=\".packages\""
+  "FLUTTER_TARGET=\"/home/gaanmori/AndroidStudioProjects/flutter_app/lib/main.dart\""
+)
